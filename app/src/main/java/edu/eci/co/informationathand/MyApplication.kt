815b7f131980a111362422536551a0d98eb6f3ed
Juanse2347 <juan.sanchez-mo@escuelaing.edu.co>
2025-11-26 13:59:39 -0500

    Adding EntradId

--- app/src/main/java/edu/eci/co/informationathand/MyApplication.kt
+++ app/src/main/java/edu/eci/co/informationathand/MyApplication.kt
@@ -1,21 +1,9 @@
 package edu.eci.co.informationathand
 
 import android.app.Application
-import android.util.Log
-import com.amplifyframework.AmplifyException
-import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
-import com.amplifyframework.core.Amplify
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        try {
-            Amplify.addPlugin(AWSCognitoAuthPlugin())
-            Amplify.configure(applicationContext)
-            Log.i("MyApplication", "Amplify inicializado correctamente")
-        } catch (error: AmplifyException) {
-            Log.e("MyApplication", "Error al inicializar Amplify", error)
-        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,28 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+
+        // Repositorio MSAL
+        maven { url = uri("https://maven.google.com") }
+        maven { url = uri("https://pkgs.dev.azure.com/microsoft/android/msal/_packaging/msal-android/maven/v1") }
+
+        // Repositorio Duo-SDK-Feed
+        maven {
+            name = "Duo-SDK-Feed"
+            url = uri("https://pkgs.dev.azure.com/MicrosoftDeviceSDK/DuoSDK-Public/_packaging/Duo-SDK-Feed/maven/v1")
+        }
     }
 }
 
-rootProject.name = "Information At Hand"
+rootProject.name = "AppMovil"
 include(":app")
